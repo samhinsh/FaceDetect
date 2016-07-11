8c38a2cffc10d4b44fc09115958c19e3d10a83b4
Samuel Hinshelwood <samhinsh@gmail.com>
2016-07-11 11:45:49 -0700

    Added faces

--- face_detect.py
+++ face_detect.py
@@ -5,12 +5,12 @@ import sys
 imagePath = sys.argv[1]
 cascPath = sys.argv[2]
 
-# Create the haar cascade
+# Create the face haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Read the image
 image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert it to gray
 
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
@@ -18,14 +18,32 @@ faces = faceCascade.detectMultiScale(
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    flags = cv2.CASCADE_SCALE_IMAGE
 )
+
+print faces
 
 print "Found {0} faces!".format(len(faces))
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # recognize the eyes 
+    eyeCascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+    eyes = eyeCascade.detectMultiScale(
+    	gray,
+    	scaleFactor=1.1,
+    	minNeighbors=5,
+    	minSize=(30, 30),
+    	flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    print eyes
+    print "Found {0} eyes!".format(len(eyes))
+
+    for (ex, ey, ew, eh) in eyes:
+    	cv2.rectangle(image, (ex, ey), (ex+ew, ey+eh), (255, 0, 200), 2)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
